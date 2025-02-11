4fb229776ec39a1620ae17e64ab2a6ed77732537
blarrise <blariise1337@gmail.com>
2025-02-11 15:13:27 +0100

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
+#include <algorithm>
 #include <iostream>
 #include <unordered_set>
 #include <vector>
 #include <sstream>
 #include <string>
+#include <filesystem>
+#include <cstdlib>
+#include <string_view>
 
 std::vector<std::string> splitCommand(const std::string& command) {
   std::vector<std::string> tokens {};
@@ -15,6 +19,18 @@ std::vector<std::string> splitCommand(const std::string& command) {
   return tokens;
 }
 
+std::vector<std::string> getPaths() {
+  const char* env_p { std::getenv("PATH") };
+  
+  if (!env_p)
+    return {};
+
+  std::string path {env_p};
+  std::replace(path.begin(), path.end(), ':', ' ');
+  return splitCommand(path);
+}
+
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -26,44 +42,54 @@ int main() {
 
     std::string input;
     std::getline(std::cin, input);
+
     std::vector<std::string> args { splitCommand(input) };
 
-    std::size_t args_num { std::size(args) };
+    if (args.empty())
+      continue;
 
-    if (args[0] == "exit" && args[1] == "0") {
-      return 0;
-    } else if (args[0] == "echo") {
-      std::string str {};
-      for (std::size_t i {1}; i < args_num; ++i) {
-        str.append(args[i]);
-	str.append(" ");
-      }
-      str.pop_back();
-      std::cout << str << '\n';
-    } else if (args[0] == "type") {
-      if (commands.contains(args[1]))
-        std::cout << args[1] << " is a shell builtin\n";
-      else
-	std::cout << args[1] << ": not found\n";
+    std::string_view cmd { args[0]};
+
+    if (!commands.contains(cmd)) {
+      std::cout << cmd << ": command not found\n";
     } else {
-      std::cout << args[0] << ": command not found\n";
-    }
 
-/*
-    if (input == "exit 0") {
-      break; 
-    } else if (std::size_t found {input.find("echo")}; found!=std::string::npos) {
-      std::cout << input.substr(5, std::size(input)) << '\n';
-    } else if (input.find("type")!=std::string::npos) {
-      std::string command {input.substr(5, std::size(input))};
-      if ( commands.find(command) != commands.end()) { 
-        std::cout << command << " is a shell builtin";
+      if (cmd == "exit" && args[1] == "0")
+        return 0;
+
+      if (cmd == "echo") {
+        std::string str {};
+        for (std::size_t i {1}; i < std::size(args); ++i) {
+          str.append(args[i]);
+          str.append(" ");
+        }
+        str.pop_back();
+        std::cout << str << '\n';
       }
-    } else {	    
-      std::cout << input << ": command not found\n";
-    }
-*/
 
+      if (cmd == "type") {
+        bool flag {false};
+
+        if (commands.contains(cmd))
+          std::cout << args[1] << " is a shell builtin\n";
+        else {
+          std::vector paths {getPaths()};
+          
+          for (auto &path : paths) {
+            const std::string full_path { path + '/' + args[1] };
+
+            if (std::filesystem::exists(full_path)) {
+              std::cout << args[1] << " is " << full_path << '\n';
+              flag = true;
+              break;
+            }
+
+          }
+        if (!flag)
+          std::cout << args[1] << ": not found\n";
+        }
+      }
+    }
   }
   return 0;
 }
